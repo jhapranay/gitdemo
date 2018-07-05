cb587f0e6aef0d52ce2020a84d4d637bf80e2a9d
OPERATIVE\pranayjha <pranayjha@sintecmedia.com>
2018-07-05 15:06:05 +0530

    test

--- src/gitdemo/Test.java
+++ src/gitdemo/Test.java
@@ -1,7 +1,9 @@
 package gitdemo;
 
 public class Test {
+	public void method32(){}
 	public static void main(String[] args) {
+		System.out.println("Testing");
 		System.out.println("Testing");
 	}
 }
